7fbb79089d4d27ad6e009a7b9d39cc15732ea7ec
Elmer Matthew Japara <elmer.japara@next-ti.co.id>
2024-05-16 15:26:45 +0700

    New update to adjust to Flutter 3.19+

---
+++ lib/src/utilities/assert_inherited_media_query.dart
@@ -0,0 +1,35 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+
+/// Indicate whether [child] uses an inherited [MediaQuery].
+///
+/// Note that its will try its best to evaluate this by seeing if child is a [WidgetsApp] and will eventually
+/// look at children if child is a [ProxyWidget], [MultiChildRenderObjectWidget] or [SingleChildRenderObjectWidget].
+/// 
+/// 
+/// note by dev elmermj:
+/// [useInheritedMediaQuery] is deprecated, therefore this file is useless but remain commented for now.
+/// 
+/// 
+// bool isWidgetsAppUsingInheritedMediaQuery(
+//   Widget child, [
+//   bool fallback = true,
+// ]) {
+//   if (child is MaterialApp) return child.useInheritedMediaQuery;
+//   if (child is CupertinoApp) return child.useInheritedMediaQuery;
+//   if (child is WidgetsApp) return child.useInheritedMediaQuery;
+
+//   if (child is ProxyWidget) {
+//     return isWidgetsAppUsingInheritedMediaQuery(child.child);
+//   }
+//   if (child is SingleChildRenderObjectWidget) {
+//     return child.child == null ||
+//         isWidgetsAppUsingInheritedMediaQuery(child.child!);
+//   }
+
+//   if (child is MultiChildRenderObjectWidget) {
+//     return child.children.every(isWidgetsAppUsingInheritedMediaQuery);
+//   }
+
+//   return fallback;
+// }
